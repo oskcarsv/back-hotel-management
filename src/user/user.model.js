c0a1e5fb69a3ba7950bf63f17485be2742190fbf
Informatica <informatica@alumno.local>
2024-04-29 10:23:19 -0600

    Model User Done

---
+++ src/user/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+
+    name: {
+
+        type: String,
+        required: [true, "Name is required"]
+
+    },
+
+    username: {
+
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+
+    },
+    
+    email: {
+
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+        
+    },
+
+    password: {
+
+        type: String,
+        required: [true, "Password is required"]
+
+    },
+
+    role: {
+
+        type: String,
+        required: true,
+        enum: ["SUPER_ROLE", "ADMIN_BOSS_ROLE", "ADMIN_EMPLOYEE", "USER_ROLE"]
+
+    },
+
+    state: {
+
+        type: Boolean,
+        default: true
+
+    }
+
+});
+
+export default mongoose.model('User', UserSchema);
