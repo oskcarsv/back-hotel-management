4e58bb1ba7204dbc35a228e1bb413de2f96aa76e
Informatica <informatica@alumno.local>
2024-04-29 10:38:04 -0600

    Default User Credentials Done

--- configs/server.js
+++ configs/server.js
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import bcryptjs from 'bcryptjs';
 import {dbConnection} from './mongo.js';
+import User from '../src/user/user.model.js'
 
 class Server{
 
@@ -17,6 +18,39 @@ class Server{
 
         this.middlewares();
         this.connectDB();
+        this.defaultCredential();
+
+    }
+
+    async defaultCredential(){
+
+        const defaultCredentials = await User.findOne({username: "admin"});
+
+        if(!defaultCredentials){
+
+
+            const defaultUser = new User({
+
+                name: "admin",
+                username: "admin",
+                password: "123456",
+                email: "admin@gmail.com",
+                role: "SUPER_ROLE"
+
+            })
+
+            const salt = bcryptjs.genSaltSync();
+            defaultUser.password = bcryptjs.hashSync(defaultUser.password, salt);
+
+            await defaultUser.save();
+
+            console.log('Default Credentials have been created.');
+
+        }else{
+
+            console.log("Default Credentials have already been created.");
+
+        }
 
     }
 
